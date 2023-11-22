d53321209d9c660e5ba16d42fe3469911852e385
raditya2010631170111 <2010631170111@student.unsika.ac.id>
2023-11-22 16:10:55 +0700

    -

--- app/Controllers/ExtendsController.php
+++ app/Controllers/ExtendsController.php
@@ -98,30 +98,6 @@ class ExtendsController extends BaseController {
 		return view('detail', $data);
 		var_dump($data); // Using var_dump for debugging
 	}
-  
-	public function createMahasiswa() {
-		$nama = $this->request->getVar("nama");
-		$npm = $this->request->getVar("npm");
-		$prodi = $this->request->getVar("prodi");
-		$minat = $this->request->getVar("minat");
-		$domisili = $this->request->getVar("domisili");
-		$jenis_kelamin = $this->request->getVar("jenis_kelamin");
-
-		$data = [
-			"nama" => $nama,
-			"npm" => $npm,
-			"prodi" => $prodi,
-			"minat" => $minat,
-			"domisili" => $domisili,
-			"jenis_kelamin" => $jenis_kelamin
-		];
-
-		$this->mahasiswaModel->create($data);
-		$this->session->setFlashData("success", "Mahasiswa has been added");
-		return redirect()->to(base_url("/"));
-		var_dump($data); // Using var_dump for debugging
-    }
-	
 	
 	public function index2(): string {
 		$data = [
